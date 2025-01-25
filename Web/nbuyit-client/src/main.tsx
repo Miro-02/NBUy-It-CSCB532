59bbc574889f4ef4f91985e7bafea7304f0e2af0
Alexander Yordanov <alexanderfjor@gmail.com>
2025-01-25 15:54:06 +0200

    Small update
    Small update

--- Web/nbuyit-client/src/main.tsx
+++ Web/nbuyit-client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import { Auth0Provider } from '@auth0/auth0-react';
 
 createRoot(document.getElementById('root')!).render(
